dcdb0256747531e6cf5ebdf1581e3f3dbe59b72c
lyman wong <lymanwong@gmail.com>
2015-11-17 09:39:58 -0800

    adding flickr links for images - about

--- inc/about.php
+++ inc/about.php
@@ -1,58 +1,59 @@
-
-
-        <div class="row">
-            <div class="box">
-                <div class="col-lg-12">
-                    <hr>
-                    <h2 class="intro-text text-center">About
-                        <strong>Me</strong>
-                    </h2>
-                    <hr>
-                </div>
-                <div class="col-md-6">
-                    <img class="img-responsive img-border-left" src="img/iceland.jpg" alt="">
-                </div>
-                <div class="col-md-6">
-                    <p>I've been extremely bless to be married to my wonderful wife, Marcela.</p>
-
-                    <p>We've traveled to Mexico, China, Iceland, France, Spain, England, Romainia to name a few. Feel free to checkout my <a href="https://www.flickr.com/photos/lyman_wong/">Flickr</a> page!</p>
-
-                    <p>I'm a huge bay area sports guy and a San Francisco 49er season ticket holder. Let's go Niners!</p>
-                </div>
-                <div class="clearfix"></div>
-            </div>
+<div class="row">
+    <div class="box">
+        <div class="col-lg-12">
+            <hr>
+            <h2 class="intro-text text-center">About
+                <strong>Me</strong>
+            </h2>
+            <hr>
         </div>
-
-        <div class="row">
-            <div class="box">
-                <div class="col-lg-12">
-                    <hr>
-                    <h2 class="intro-text text-center">Our
-                        <strong>Family</strong>
-                    </h2>
-                    <hr>
-                </div>
-                <div class="col-sm-4 text-center">
-                    <img class="img-responsive" src="/img/niners.jpg/750x450" alt="">
-                    <h3>Marcela Benitez<br />
-                        <small>Community Manager<br />
-                        @CBS Interactive</small>
-                    </h3>
-                </div>
-                <div class="col-sm-4 text-center">
-                    <img class="img-responsive" src="/img/lifting.jpg/750x450" alt="">
-                    <h3>Lyman Wong<br />
-                        <small>Full Stack Developer<br />
-                        @Phlint</small>
-                    </h3>
-                </div>
-                <div class="col-sm-4 text-center">
-                    <img class="img-responsive" src="/img/freyja_1.jpg/750x450" alt="">
-                    <h3>Freyja<br />
-                        <small>Chief Fun Officer<br />
-                        @everywhere</small>
-                    </h3>
-                </div>
-                <div class="clearfix"></div>
-            </div>
+        <div class="col-md-6">
+            <img class="img-responsive img-border-left" src="img/iceland.jpg" alt="">
         </div>
+        <div class="col-md-6">
+            <p>I've been extremely bless to be married to my wonderful wife, Marcela.</p>
+
+            <p>We've traveled to Mexico, China, Iceland, France, Spain, England, Romainia to name a few. Feel free to checkout my <a href="https://www.flickr.com/photos/lyman_wong/">Flickr</a> page!</p>
+
+            <p>I'm a huge bay area sports guy and a San Francisco 49er season ticket holder. Let's go Niners!</p>
+        </div>
+        <div class="clearfix"></div>
+    </div>
+</div>
+
+<div class="row">
+    <div class="box">
+        <div class="col-lg-12">
+            <hr>
+            <h2 class="intro-text text-center">Our
+                <strong>Family</strong>
+            </h2>
+            <hr>
+        </div>
+        <div class="col-sm-4 text-center">
+            <a data-flickr-embed="true"  href="https://www.flickr.com/photos/lyman_wong/22471704214/in/datetaken/" title="niners"><img class="img-responsive"src="https://farm6.staticflickr.com/5830/22471704214_23f2ff28f4_b.jpg" width="960" height="720" alt="niners"></a><script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>
+            <!-- <img class="img-responsive" src="/img/niners.jpg/750x450" alt=""> -->
+            <h3>Marcela Benitez<br />
+                <small>Community Manager<br />
+                @CBS Interactive</small>
+            </h3>
+        </div>
+        <div class="col-sm-4 text-center">
+            <a data-flickr-embed="true"  href="https://www.flickr.com/photos/lyman_wong/22676134537/in/datetaken/" title="lifting"><img class="img-responsive" src="https://farm6.staticflickr.com/5815/22676134537_263884d18d_b.jpg" width="631" height="705" alt="lifting"></a><script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>
+            <!-- <img class="img-responsive" src="/img/lifting.jpg/750x450" alt=""> -->
+            <h3>Lyman Wong<br />
+                <small>Front End Developer<br />
+                @Phlint</small>
+            </h3>
+        </div>
+        <div class="col-sm-4 text-center">
+            <a data-flickr-embed="true"  href="https://www.flickr.com/photos/lyman_wong/22702217329/in/datetaken/" title="Freyja_1"><img class="img-responsive" src="https://farm1.staticflickr.com/731/22702217329_9bc39a983d_k.jpg" width="2048" height="1534" alt="Freyja_1"></a><script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>
+            <!-- <img class="img-responsive" src="/img/freyja_1.jpg/750x450" alt=""> -->
+            <h3>Freyja<br />
+                <small>Chief Fun Officer<br />
+                @everywhere</small>
+            </h3>
+        </div>
+        <div class="clearfix"></div>
+    </div>
+</div>
