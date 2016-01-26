1cd9fa77a17bc2f1f08b2434fd7b03a2b3448452
lyman wong <lymanwong@gmail.com>
2016-01-26 10:45:02 -0800

    add social media links

--- inc/footer.php
+++ inc/footer.php
@@ -2,8 +2,16 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-12 text-center">
-                <p>Copyright &copy; Lyman Wong 2015</p>
+                <p class="copyright">Copyright &copy; Lyman Wong 2015</p>
             </div>
+        </div>
+        <div class="row">
+          <ul class="social">
+            <li class="linkedin"><a href="https://www.linkedin.com/in/lymanwong" target="_blank"><i class="fa fa-linkedin-square fa-lg"></i></a></li>
+            <li class="twitter"><a href="https://twitter.com/lyman_g_wong" target="_blank"><i class="fa fa-twitter-square fa-lg"></i></a></li>
+            <li class="instagram"><a href="https://www.instagram.com/lymanwong/" target="_blank"><i class="fa fa-instagram fa-lg"></i></a></li>
+            <li class="flickr" target="_blank"><a href="https://www.flickr.com/photos/lyman_wong/"><i class="fa fa-flickr fa-lg"></i></a></li>
+          </ul>
         </div>
     </div>
 </footer>
